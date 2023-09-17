5888eacd99be7e3755fcdf3b34ef449aeba39da4
Adrien Friggeri <adrien@friggeri.net>
2023-09-16 18:49:39 -0600

    Initial Commit

---
+++ CameraPanTilt/CameraPanTiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraPanTiltApp.swift
+//  CameraPanTilt
+//
+//  Created by Adrien Friggeri on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraPanTiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
